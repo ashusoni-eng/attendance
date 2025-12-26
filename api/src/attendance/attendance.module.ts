59fa9197b61d8c9035240db222d8f20683e8b190
Ankit <rankit242@gmail.com>
2025-12-26 11:38:02 +0530

    wip

--- api/src/attendance/attendance.module.ts
+++ api/src/attendance/attendance.module.ts
@@ -1,19 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AttendanceService } from './attendance.service';
-<<<<<<< Updated upstream
 // import { AttendanceController } from './attendance.controller';
-=======
->>>>>>> Stashed changes
 import { AuthModule } from 'src/auth/auth.module';
 import { BlacklistModule } from 'src/blacklist/blacklist.module';
 
 @Module({
   imports:[AuthModule,BlacklistModule],
-<<<<<<< Updated upstream
   // controllers: [AttendanceController],
-=======
-  controllers: [],
->>>>>>> Stashed changes
   providers: [AttendanceService],
   exports:[AttendanceService]
 })
--- api/src/users/admin/admin.service.ts
+++ api/src/users/admin/admin.service.ts
@@ -27,17 +27,18 @@ import { LeaveTypeService } from "src/leaves/leave-type/leave-type.service";
 import { PublicHolidaysService } from "src/public-holidays/public-holidays.service";
 import { UpdateLeaveRequestDto } from "src/leaves/leave-requests/dto/update-leave-requests.dto";
 import { CreateLeaveTypeDto } from "src/leaves/leave-type/create-leave-type.dto";
+import { CreateLeaveEntitlementDto } from "src/leaves/leave-entitlements/dto/create-leave-entitlements.dto";
 
 @Injectable()
 export class AdminService {
   constructor(
     private prisma: PrismaService,
     private readonly configService: ConfigService,
-    private attendanceService:AttendanceService,
-    private leaveRequestService:LeaveRequestService,
-    private leaveEntitlementService:LeaveEntitlementService,
-    private leaveTypeService:LeaveTypeService,
-    private publicHolidayService:PublicHolidaysService
+    private attendanceService: AttendanceService,
+    private leaveRequestService: LeaveRequestService,
+    private leaveEntitlementService: LeaveEntitlementService,
+    private leaveTypeService: LeaveTypeService,
+    private publicHolidayService: PublicHolidaysService
   ) { }
 
   async create(createAdminDto: CreateAdminDto) {
@@ -78,10 +79,10 @@ export class AdminService {
     });
   }
 
-  
 
-  async findAll(page: number, perPage: number,query:string, from: Date|undefined, to: Date) {
-    return await this.attendanceService.findAll(page,perPage,query,from,to)
+
+  async findAll(page: number, perPage: number, query: string, from: Date | undefined, to: Date) {
+    return await this.attendanceService.findAll(page, perPage, query, from, to)
     // try {
     //   const { skip, take } = getPaginationOptions({ page, perPage });
     //   const where = { AND: [{}] };
@@ -116,29 +117,30 @@ export class AdminService {
     // });
   }
 
-  async findAllLeaveRequest(requestStatus:RequestStatus|undefined,page:number,perPage:number,from:Date|undefined,to:Date){
-    return this.leaveRequestService.findAllByAdmin(requestStatus,page,perPage,from,to)
+  async findAllLeaveRequest(requestStatus: RequestStatus | undefined, page: number, perPage: number, from: Date | undefined, to: Date) {
+    return this.leaveRequestService.findAllByAdmin(requestStatus, page, perPage, from, to)
   }
 
-  async updateLeaveRequest(updateLeaveRequest:UpdateLeaveRequestDto){
+  async updateLeaveRequest(updateLeaveRequest: UpdateLeaveRequestDto) {
     return this.leaveRequestService.updateByAdmin(updateLeaveRequest)
   }
 
-  async createLeaveType(createLeaveTypeDto:CreateLeaveTypeDto){
+  async createLeaveType(createLeaveTypeDto: CreateLeaveTypeDto) {
     return this.leaveTypeService.create(createLeaveTypeDto)
   }
-  
-  async findAllLeaveType(){
+
+  async findAllLeaveType() {
     return this.leaveTypeService.findAll()
   }
-  async assignLeaveToEmployee(){
-    return this.leaveEntitlementService.create()
+  async assignLeavesToEmployee(createLeaveEntitlementDto: CreateLeaveEntitlementDto) {
+
+    return this.leaveEntitlementService.create(createLeaveEntitlementDto)
   }
 
 
 
-  async findOne(id: string,page:number,perPage:number,from:string|undefined,to:string|undefined) {
-    return await this.attendanceService.findById(id,page,perPage,from,to)
+  async findOne(id: string, page: number, perPage: number, from: string | undefined, to: string | undefined) {
+    return await this.attendanceService.findById(id, page, perPage, from, to)
     // const user = await this.prisma.user.findUnique({
     //   where: { id },
     // });
